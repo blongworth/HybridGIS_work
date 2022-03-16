d11dda4540406ccadec6b0211ca44b86ba6196c0
Brett Longworth <blongworth@whoi.edu>
2022-03-16 16:58:36 -0400

    Drop high blank from early wheel. Notes confirm that this sample was suspect.

--- R/analyse_carbonates.R
+++ R/analyse_carbonates.R
@@ -15,7 +15,7 @@ wheels <- c("020521", "030421", "040121", "041521", "051421", "052621", "061121"
 files <- here("data", paste0("USAMS", wheels, "R.txt"))
 dates <- as.Date(c("2021-02-05", "2021-03-05", "2021-04-09", "2021-04-16", "2021-05-14", "2021-05-28", "2021-06-11", "2021-06-18", "2021-06-25", "2021-07-02"))
 stds <- list(c(5,8), NULL, c(26,37), NULL, c(10,15,65,70), NULL, NULL, NULL, NULL, NULL)
-blanks <- list(7, c(7,8), c(28,29,30), c(27,28,29), c(20,35,50), NULL, NULL, 23, NULL, NULL)
+blanks <- list(7, c(8), c(28,29,30), c(27,28,29), c(20,35,50), NULL, NULL, 23, NULL, NULL)
 outliers <- list(NULL, tibble(pos = c(6, 8, 8, 8), meas = c(18, 1, 2, 3)),
                  tibble(pos = c(28,29), meas=c(1, 1)),
                  NULL,

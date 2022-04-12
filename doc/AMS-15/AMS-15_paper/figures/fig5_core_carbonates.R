6cbdfb32438bc37ca73eec7d5ed22cd994191abe
Brett Longworth <blongworth@whoi.edu>
2022-04-12 17:41:28 -0400

    Add combined age models and model difference.

--- doc/AMS-15/AMS-15_paper/figures/fig5_core_carbonates.R
+++ doc/AMS-15/AMS-15_paper/figures/fig5_core_carbonates.R
@@ -91,31 +91,31 @@ core_compare <- ggplot(cr_no) +
        x = "Mean of measurements (F14C)",
        y = "HGIS - graphite (F14C)") +
   theme(legend.position = "none")
-  # theme(legend.position = c(0.85, 0.15),
-  #       legend.direction = "horizontal",
-  #       legend.background = element_rect(fill = "white", color = "black")) 
+  # theme(legend.position = c(0.25, 0.85),
+  #       legend.direction = "horizontal") #,
+  #       #legend.background = element_rect(fill = "white", color = "black")) 
 
 
 # Simple age depth plot
 # If this is used, should use calibrated ages
 # or at least bomb correction
 
-age_depth <- cr_no %>% 
-  ggplot(aes(depth, rc_age, color = method, shape = method)) +
-  geom_pointrange(aes(ymin = rc_age - sig_rc_age, 
-                      ymax = rc_age + sig_rc_age),
-                  size = 0.5) +
-  geom_smooth(alpha = .2) +
-  coord_flip() +
-  scale_y_reverse() +
-  scale_x_reverse() +
-  scale_color_manual(values = c("#00b7bd", "#b7bf10")) +
-  labs(title = "B",
-       #subtitle = "Age vs. depth for graphite and HGIS",
-       y = "Radiocarbon years (BP)",
-       x = "Core depth (cm)") +
-  theme(legend.position = c(0.85, 0.30),
-        legend.background = element_rect(fill = "white", color = "black")) 
+# age_depth <- cr_no %>% 
+#   ggplot(aes(depth, rc_age, color = method, shape = method)) +
+#   geom_pointrange(aes(ymin = rc_age - sig_rc_age, 
+#                       ymax = rc_age + sig_rc_age),
+#                   size = 0.5) +
+#   geom_smooth(alpha = .2) +
+#   coord_flip() +
+#   scale_y_reverse() +
+#   scale_x_reverse() +
+#   scale_color_manual(values = c("#00b7bd", "#b7bf10")) +
+#   labs(title = "B",
+#        #subtitle = "Age vs. depth for graphite and HGIS",
+#        y = "Radiocarbon years (BP)",
+#        x = "Core depth (cm)") +
+#   theme(legend.position = c(0.85, 0.30),
+#         legend.background = element_rect(fill = "white", color = "black")) 
 
 # Bchron age models
 
@@ -124,46 +124,141 @@ age_depth <- cr_no %>%
 chron_hgis <- read_rds(here("data_analysed/hgis_chronology.rds"))
 chron_gr <- read_rds(here("data_analysed/graphite_chronology.rds"))
 
-hgis_bchron <- plot(chron_hgis, dateLabels = FALSE) +
+# Individual plots of HGIS and graphite models
+hgis_bchron <- plot(chron_hgis, chronCol = "#00b7bd", dateLabels = FALSE) +
   xlim(8000, -500) +
   labs(title = "B",
-       y = "Years BP",
-       x = "Core depth (cm)")
+       x = "Years BP",
+       y = "Core depth (cm)")
 
-gr_bchron <- plot(chron_gr, dateLabels = FALSE) +
+gr_bchron <- plot(chron_gr, chronCol = "#b7bf10", dateLabels = FALSE) +
   xlim(8000, -500) +
   labs(title = "C",
-       y = "Years BP",
-       x = "Core depth (cm)") +
+       x = "Years BP",
+       y = "Core depth (cm)") +
   theme(axis.title.y = element_blank(),
         axis.text.y = element_blank())
+
+# Generate hgis dataframe for combined plot
+alpha <- 0.95
+chronRange <- data.frame(
+  chronLow = apply(chron_hgis$thetaPredict, 2, "quantile", probs = (1 - alpha) / 2),
+  chronMed = apply(chron_hgis$thetaPredict, 2, "quantile", probs = 0.5),
+  chronHigh = apply(chron_hgis$thetaPredict, 2, "quantile", probs = 1 - (1 - alpha) / 2),
+  positions = chron_hgis$predictPositions
+)
+ageGrid <- with(chronRange, seq(min(chronLow), max(chronHigh),
+                                length = nrow(chronRange)
+))
+chronRangeSwap_hgis <- data.frame(
+  Age = ageGrid,
+  positionLow = with(chronRange, approx(chronLow, positions,
+                                        xout = ageGrid,
+                                        rule = 2
+  )$y),
+  Position = with(chronRange, approx(chronMed, positions,
+                                     xout = ageGrid,
+                                     rule = 2
+  )$y),
+  positionHigh = with(chronRange, approx(chronHigh, positions,
+                                         xout = ageGrid,
+                                         rule = 2
+  )$y),
+  Date = "Bchron",
+  densities = NA,
+  height = NA
+)
+
+# Generate graphite dataframe for combined plot
+alpha <- 0.95
+chronRange <- data.frame(
+  chronLow = apply(chron_gr$thetaPredict, 2, "quantile", probs = (1 - alpha) / 2),
+  chronMed = apply(chron_gr$thetaPredict, 2, "quantile", probs = 0.5),
+  chronHigh = apply(chron_gr$thetaPredict, 2, "quantile", probs = 1 - (1 - alpha) / 2),
+  positions = chron_gr$predictPositions
+)
+# ageGrid <- with(chronRange, seq(min(chronLow), max(chronHigh),
+#                                 length = nrow(chronRange)
+# ))
+chronRangeSwap <- data.frame(
+  Age = ageGrid,
+  positionLow = with(chronRange, approx(chronLow, positions,
+                                        xout = ageGrid,
+                                        rule = 2
+  )$y),
+  Position = with(chronRange, approx(chronMed, positions,
+                                     xout = ageGrid,
+                                     rule = 2
+  )$y),
+  positionHigh = with(chronRange, approx(chronHigh, positions,
+                                         xout = ageGrid,
+                                         rule = 2
+  )$y),
+  Date = "Bchron",
+  densities = NA,
+  height = NA
+)
+
+# Combine data, add mean and difference
+chronRangeSwap_hg <- chronRangeSwap_hgis %>% 
+  mutate(method = "hgis")
+chronRangeSwap_gr <- chronRangeSwap %>% 
+  mutate(method = "graphite")
+chron_comb <- bind_rows(chronRangeSwap_hg, chronRangeSwap_gr)
+
+chron_comb_mean <- chron_comb %>% 
+  pivot_wider(id_cols = Age, names_from = method, values_from = c(Position, positionLow, positionHigh)) %>% 
+  mutate(pos_mean = (Position_hgis + Position_graphite) / 2) %>% 
+  select(Age, pos_mean)
+
+chron_comb <- chron_comb %>% 
+  left_join(chron_comb_mean) %>% 
+  mutate(pos_diff = Position - pos_mean,
+         pos_low_diff = positionLow - pos_mean,
+         pos_high_diff = positionHigh - pos_mean)
+
+# Combined plot, no age histograms
+chron_combined <- ggplot(chron_comb,
+       aes(x = Age, y = Position,
+           ymin = positionLow, ymax = positionHigh, 
+           color = method, fill = method)) +
+  geom_ribbon(alpha = 0.4, color = NA) +
+  geom_line(size = 1) +
+  xlim(8000, -500) +
+  labs(title = "D",
+       x = "Years BP",
+       y = "Core depth (cm)") +
+  scale_color_manual(values = c("#00b7bd", "#b7bf10")) +
+  scale_fill_manual(values = c("#00b7bd", "#b7bf10")) +
+  scale_y_reverse() +
+  # theme(legend.position = "none")
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        legend.position = c(0.25, 0.80),
+        legend.direction = "horizontal",
+        legend.background = element_rect(fill = "white", color = "black")) 
+
+# Difference plot
+chron_diff <- ggplot(chron_comb, aes(Age, pos_diff, color = method,
+                       ymin = pos_low_diff, ymax = pos_high_diff, 
+                       fill = method)) +
+  geom_hline(yintercept = 0) +
+  geom_ribbon(alpha = 0.4, color = NA) +
+  geom_line(size = 1) +
+  scale_color_manual(values = c("#00b7bd", "#b7bf10")) +
+  scale_fill_manual(values = c("#00b7bd", "#b7bf10")) +
+  scale_x_reverse() +
+  xlim(8000, -500) +
+  labs(title = "E",
+       x = "Years BP",
+       y = "Offset (cm)") +
+  theme(legend.position = "none")
+
 # Combine plots and save
 
-core_compare / (hgis_bchron | gr_bchron)
+core_compare / (hgis_bchron | gr_bchron) / chron_combined / chron_diff +
+  plot_layout(heights = c(2, 3, 3, 1))
 
 # Save figure
-ggsave(here("doc/AMS-15/AMS-15_paper/figures/fig5_core_carbonates.svg"), width = 7, height = 7)
-ggsave(here("doc/AMS-15/AMS-15_paper/figures/fig5_core_carbonates.pdf"), width = 7, height = 7)
-
-# data for core carbonates section
-
-# mean 
-
-# cr_no %>% 
-#   group_by(method) %>% 
-#   #filter(method == "hgis") %>% 
-#   summarize(across(c(sig_fm_corr, fm_diff),
-#                    list(mean = mean, sd = sd)),
-#             N = n()) %>% 
-#   select(`Consensus difference` = fm_diff_mean,
-#          `Std. Dev. of consensus difference` = fm_diff_sd,
-#          `per-sample error` = sig_fm_corr_mean,
-#          N
-#          ) %>%  
-#   gt() %>% 
-#   tab_header(title = "Haiti carbonates performance summary",
-#              subtitle = "TIRI-I, C-2, and NOSAMS2. Values in pMC") %>% 
-#   fmt_number(1:3, 
-#              scale_by = 100,
-#              decimals = 2)
-# 
+ggsave(here("doc/AMS-15/AMS-15_paper/figures/fig5_core_carbonates.svg"), width = 7, height = 10)
+ggsave(here("doc/AMS-15/AMS-15_paper/figures/fig5_core_carbonates.pdf"), width = 7, height = 10)

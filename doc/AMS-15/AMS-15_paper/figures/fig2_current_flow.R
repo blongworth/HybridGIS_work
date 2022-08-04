ffeab415f572bd517fd5bee83eade9abdbbafa9f
Brett Longworth <blongworth@whoi.edu>
2022-08-04 14:11:10 -0400

    Add ratio plot, break out current flow figure

--- doc/AMS-15/AMS-15_paper/figures/fig2_current_flow.R
+++ doc/AMS-15/AMS-15_paper/figures/fig2_current_flow.R
@@ -31,6 +31,18 @@ cur_time <- ggplot(data, aes(time, he12C)) +
   theme(axis.title.x = element_blank(),
         axis.text.x = element_blank())
 
+# ratio vs time subplot
+cur_rat <- ggplot(data, aes(time, he14_12)) +
+  geom_smooth(span = .4, se = FALSE, color = "#00b7bd") +
+  geom_point(size = 3, color = "#0069b1") +
+  xlim(0, 125) +
+  labs(title = "B",
+       #title = "Ion current",
+       x = "Time (min)",
+       y = TeX(r"( $^\frac{14}{12}C^-$ ratio )")) +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank())
+
 # flow vs time subplot
 flow_time <- data.frame(x = 0:125) %>% 
   ggplot(aes(x)) +
@@ -39,7 +51,7 @@ flow_time <- data.frame(x = 0:125) %>%
   stat_function(fun = function(x) 30 - (concCO2(x, r = 244) * 30),
                 aes(color = "Helium"), size = 1.5) +
   scale_color_manual("Gas", values = c("#00b7bd", "#b7bf10")) +
-  labs(title = "B",
+  labs(title = "C",
        #title = "Gas flows to source",
        #subtitle = "7mL vial, 244μL/min helium, 30μL/min to source",
        x = "Time (min)",
@@ -48,16 +60,20 @@ flow_time <- data.frame(x = 0:125) %>%
         legend.background = element_rect(fill = "white", color = "black"))
 
 # Current vs flow subplot
-
 cur_flow <- ggplot(data, aes(co2flow, he12C)) +
   geom_smooth(span = .4, se = FALSE, color = "#00b7bd") +
   geom_point(size = 3, color = "#0069b1") +
-  labs(title = "C",
+  labs(title = "D",
        x = TeX(r"($CO^2$ flow (μl $min^{-1}$))"),
        y = TeX(r"( $^{12}C^-$ current (μA))")) 
 
 # Build figure and save
-cur_time / flow_time / cur_flow
+cur_time / cur_rat / flow_time
 
 ggsave(here("doc/AMS-15/AMS-15_paper/figures/fig2_current_flow.svg"), width = 7, height = 7)
 ggsave(here("doc/AMS-15/AMS-15_paper/figures/fig2_current_flow.pdf"), width = 7, height = 7)
+
+cur_flow
+
+ggsave(here("doc/AMS-15/AMS-15_paper/figures/fig3_current_flow.svg"), width = 7, height = 7)
+ggsave(here("doc/AMS-15/AMS-15_paper/figures/fig3_current_flow.pdf"), width = 7, height = 7)
